665d2ecfa4ace89ca9905af28e8ee54ab9f26ecc
Rebilion <rebelionpw@gmail.com>
2019-08-22 16:00:04 -0300

    primeiro commit

---
+++ meat-app-starter-master/src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Restaurant} from'./restaurant/restaurant.model'
+import {RestaurantsService} from'./restaurants.services'
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[]
+
+  constructor(private RestaurantsService: RestaurantsService) { }
+
+  ngOnInit() {
+    this.restaurants = this.RestaurantsService.restaurants()
+  }
+
+}
